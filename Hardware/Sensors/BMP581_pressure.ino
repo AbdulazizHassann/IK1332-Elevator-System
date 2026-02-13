8484e5c50bdadda5089f39b9e9b82e3485d8634b
MickhelSumarlie <mick0001@student.monash.edu>
2026-02-13 01:19:34 +0100

    Update BMP581_pressure.ino

--- Hardware/Sensors/BMP581_pressure.ino
+++ Hardware/Sensors/BMP581_pressure.ino
@@ -3,41 +3,33 @@
 
 BMP581 bmp;
 
-// Your chosen I2C pins
-#define SDA_PIN 3
-#define SCL_PIN 4
+#define SDA_PIN 2
+#define SCL_PIN 1
 
 void setup() {
     Serial.begin(115200);
     delay(500);
 
     Serial.println("Starting I2C on custom pins...");
-    Wire.begin(SDA_PIN, SCL_PIN);   // SDA = 3, SCL = 4
+    Wire.begin(SDA_PIN, SCL_PIN);
 
     Serial.println("Initializing BMP581...");
-    if (!bmp.beginI2C()) {
-        Serial.println("ERROR: BMP581 not detected. Check wiring!");
-        while (1) {
-            delay(100);
-        }
+    if (bmp.beginI2C(0x47, Wire) != BMP5_OK) {
+        Serial.println("ERROR: BMP581 not detected!");
+        while (1) delay(100);
     }
 
     Serial.println("BMP581 initialized successfully!");
 }
 
 void loop() {
-    bmp5_sensor_data data;   // Struct to hold sensor readings
-
-    // IMPORTANT: pass pointer using &data
+    bmp5_sensor_data data;
     bmp.getSensorData(&data);
 
-    float pressure = data.pressure;         // Pascals
-    float temperature = data.temperature;   // Celsius
-
     Serial.print("Pressure (Pa): ");
-    Serial.print(pressure);
+    Serial.print(data.pressure);
     Serial.print("   Temperature (C): ");
-    Serial.println(temperature);
+    Serial.println(data.temperature);
 
     delay(300);
 }
